5c19d0fede0d26b136c81a8ba855e63bc40291d7
BreathInTilt <91969572+BreathInTilt@users.noreply.github.com>
2022-04-23 08:55:41 +0000

    Updating Alarm.py, adding gui

--- Alarm.py
+++ Alarm.py
@@ -1,5 +1,7 @@
 import datetime
-
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+import sys
 
 def alarm():
     alarm_hour = int(input("Введите час = "))
@@ -12,5 +14,29 @@ def alarm():
             if alarm_min == now.minute:
                 if alarm_sec == now.second:
                     print("Подъем!!!")
+                    print("Подъем!!!")
+                    print("Подъем!!!")
+                    break
 
-alarm()
+
+def window():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(200,200,300,300) 
+    win.setWindowTitle("My first window!") 
+    
+    label = QLabel(win)
+    label.setText("my first label")
+    label.move(50, 50)  
+
+    win.show()
+    sys.exit(app.exec_())
+
+
+def main():
+    #alarm()
+    window()
+
+
+if __name__ == "__main__":
+    main()

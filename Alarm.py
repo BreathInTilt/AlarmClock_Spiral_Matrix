29db9bcc7a6721d4859dc70e373598fe6e6a22c0
BreathInTilt <you.path.e2e4@gmail.com>
2022-05-04 17:56:20 +0300

    structure edited

--- Alarm.py
+++ Alarm.py
@@ -8,125 +8,133 @@ from threading import Thread
 
 
 <<<<<<< HEAD
+class Alarm:
 
-
-def timer_count(al_h, al_m, al_s):
-    now = datetime.datetime.now()
-    n_s = now.second
-    n_m = now.minute
-    n_h = now.hour
-    if al_s - n_s < 0:
-        al_m -= 1
-        al_s += 60
-    if al_m - n_m < 0:
-        al_h -= 1
-        al_m += 60
-    if al_h - n_h < 10:   a = "0" + str(al_h - n_h)
-    else:   a = str(al_h - n_h)
-    if al_m - n_m < 10:   b = "0" + str(al_m - n_m)
-    else:   b = str(al_m-n_m)
-    if al_s - n_s < 10:   c = "0" + str(al_s - n_s)
-    else:   c = str(al_s - n_s)
-    timer = a + ':' + b + ':' + c
-    return timer
-
-def sound():
-    path2 = path.dirname(path.abspath(__file__))
-    playsound(path2 + "\\rota_podeoom.mp3")
-
-def valuecheck(al_h, al_m, al_s):
-    s = al_h + al_m + al_s    
-    for i in s:
-        if 46 > ord(i) or ord(i) > 57:
-            return False
-    return True
-    
-
-
-def time_check(al_h, al_m, al_s):
-    al_h = int(al_h)
-    al_m = int(al_m)
-    al_s = int(al_s)
-    now = datetime.datetime.now()
-    if 24 > al_h > now.hour:
-        if 60 > al_m > 0:
-            if 60 > al_s > 0:
-                return True
-    elif 24 > al_h == now.hour:
-        if 60 > al_m > now.minute:
-            if 60 > al_s > now.second:
-                return True
-    return False
-
-
-
-
-
-def get_alarm():
-    window = Tk()  
-    window.title("Input")  
-    window.geometry('400x250')  
-    lbl = Label(window, text="Будильник:")
-    lbl.grid(column=0, row=0)
-    txt_h = Entry(window,width=10)
-    txt_h.grid(column=0, row=10)
-    txt_m = Entry(window,width=10)
-    txt_m.grid(column=0, row=20)
-    txt_s = Entry(window,width=10)
-    txt_s.grid(column=0, row=30)
-
-    def cleanup():
-        btn.destroy()
-        lbl.destroy()
-        txt_h.destroy()
-        txt_m.destroy()
-        txt_s.destroy()
-
-
-    def clicked():
-        al_h, al_m, al_s = txt_h.get(), txt_m.get(), txt_s.get()
-        if valuecheck(al_h, al_m, al_s) and time_check(al_h, al_m, al_s):
-            cleanup()
-            alarm(int(al_h), int(al_m), int(al_s), window)
-        else:
-            lbl.configure(text="Введите корректное значение!")
-
-    btn = Button(window, text="Старт!", command=clicked)
-    btn.grid(column=2, row=10)
-
-    window.mainloop()
-
-
-
-
-def alarm(alarm_hour, alarm_min, alarm_sec, window):
-    #alarm_hour = int(input("Введите час = "))
-    #alarm_min = int(input("Введите минуты = "))
-    #alarm_sec = int(input("Введите секунды = "))
-    
-    timeremaining = StringVar()
-    timelabel = Label(window, textvariable=timeremaining, anchor=CENTER, width=10, height=5, font=("Arial", 20)) #Design
-    timeremaining.set("Tap to update")
-    timelabel.grid(column=60, row=10)   #Design
-
-    def count_down():
-     while True:
-        now = datetime.datetime.now()
+    def __init__(self):
+        self.path2 = path.dirname(path.abspath(__file__))
+        self.window = Tk()  
+        self.window.title("Input")  
+        self.window.geometry('480x480')
+        self.window["background"] = "blue"
+        self.lbl = LabelFrame(self.window, text="Будильник:")
+        self.lbl.pack(side=TOP)
+        self.lbl.grid(column=0, row=0)
+        self.label_h = Label(self.window, text="Часы:", bg="blue", font=("OpenSansBold", 14), fg="white")
+        self.label_h.place(x=5, y=20)
+        self.txt_h = Entry(self.window,width=10)
+        self.txt_h.place(x=95, y=25)
+        self.label_m = Label(self.window, text="Минуты:", bg="blue", font=("OpenSansBold", 14), fg="white")
+        self.label_m.place(x=5, y=45)
+        #txt_h.grid(column=10, row=10).pack
+        self.txt_m = Entry(self.window,width=10)
+        self.txt_m.place(x=95, y=50)
+        #txt_m.grid(column=10, row=20)
+        self.label_s = Label(self.window, text="Секунды:", bg="blue", font=("OpenSansBold", 14), fg="white")
+        self.label_s.place(x=5, y=70)
+        self.txt_s = Entry(self.window,width=10)
+        self.txt_s.place(x=95, y=75)
+        img = PhotoImage(file = self.path2 + "\\gobtn.png")    
+        self.btn = Button(self.window, command=self.clicked, image=img, width=100, height=100)
+        self.btn.place(x = 180, y = 25)
+        self.timelabel = Label(self.window, bg="blue", anchor=CENTER, width=10, height=5, font=("OpenSansBold", 14), fg="white")
+        #btn.image = img
+        #self.warn = LabelFrame(self.window)
+        #self.warn.pack(side=BOTTOM)
+        self.window.mainloop()
         
-        if alarm_hour == now.hour:
-            if alarm_min == now.minute:
-                if alarm_sec <= now.second:
-                    timeremaining.set("Wake up!")
-                    Thread(target = sound, daemon=True).start()
-                    break
-        timeremaining.set(timer_count(alarm_hour, alarm_min, alarm_sec))
-        sleep(0.5)
-        window.update()
+    def valuecheck(self):
+        s = self.al_h + self.al_m + self.al_s    
+        for i in s:
+            if 46 > ord(i) or ord(i) > 57:
+                return False
+        return True
+        
 
-            
-    
-    Button(window, text="Tap", command=count_down, height=5, width=5).grid(column=150, row=200)  #Design
-    
 
-#alarm()
-get_alarm()
+    def time_check(self):
+        self.al_h = int(self.al_h)
+        self.al_m = int(self.al_m)
+        self.al_s = int(self.al_s)
+        now = datetime.datetime.now()
+        if 24 > self.al_h > now.hour:
+            return True
+        elif 24 > self.al_h == now.hour:
+            if 60 > self.al_m > now.minute:
+                return True
+        return False
+
+    def clicked(self):
+        self.al_h, self.al_m, self.al_s = self.txt_h.get(), self.txt_m.get(), self.txt_s.get()
+        if self.valuecheck():
+            if self.time_check():
+                self.cleanup()
+                self.timelabel = Label(self.window, text="Tap to update", anchor=CENTER, width=10, height=5, font=("OpenSansBold", 20),fg="white" , bg="blue") #Design
+                self.timelabel.grid(column=100, row=60)   #Design
+                self.count_down()
+            else:
+                self.lbl.configure(text="Введите корректное значение!")
+        else:
+            self.lbl.configure(text="Введите корректное значение!")
+
+    def time_set(self):
+        
+        now = datetime.datetime.now()
+        n_s = now.second
+        n_m = now.minute
+        n_h = now.hour
+        alarm_sec = self.al_s
+        alarm_min = self.al_m
+        alarm_hour = self.al_h
+        if alarm_sec - n_s < 0:
+            alarm_min -= 1
+            alarm_sec += 60
+        if alarm_min - n_m < 0:
+            alarm_hour -= 1
+            alarm_min += 60
+        if alarm_hour - n_h < 10:   a = "0" + str(alarm_hour - n_h)
+        else:   a = str(alarm_hour - n_h)
+        if alarm_min - n_m < 10:   b = "0" + str(alarm_min - n_m)
+        else:   b = str(alarm_min - n_m)
+        if alarm_sec - n_s < 10:   c = "0" + str(alarm_sec - n_s)
+        else:   c = str(alarm_sec - n_s)
+        timer = a + ':' + b + ':' + c
+        return timer
+
+
+    def count_down(self):
+        while True:
+            now = datetime.datetime.now()
+                
+            if self.al_h == now.hour:
+                if self.al_m == now.minute:
+                    if self.al_s <= now.second:
+                        #self.timeremaining.set("Wake up!")
+                        self.timelabel.config(text="Wake up!")
+                        Thread(target = self.sound, daemon=True).start()
+                        break
+            self.timelabel.config(text=self.time_set())
+            sleep(0.5)
+            self.window.update()
+                    
+
+    def cleanup(self):
+        self.label_h.destroy()
+        self.label_m.destroy()
+        self.label_s.destroy()
+        self.btn.destroy()
+        self.lbl.destroy()
+        self.txt_h.destroy()
+        self.txt_m.destroy()
+        self.txt_s.destroy()
+
+    def sound(self):
+        playsound(self.path2 + "\\rota_podeoom.mp3")
+
+    def alarm(self):
+        self.timelabel.place(x=10, y=10)
+        self.count_down()
+        #self.time_set()
+
+
+Alarm()
+>>>>>>> testing
